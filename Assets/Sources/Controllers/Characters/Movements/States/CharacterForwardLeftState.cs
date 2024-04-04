3e8d42eb880edc6d1c3efa2bf576780d9530f8db
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-04-04 23:35:42 +0300

    add characterMovementStateMachine

---
+++ Assets/Sources/Controllers/Characters/Movements/States/CharacterForwardLeftState.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Sources.Domain.Characters;
+using Sources.Infrastructure.StateMachines.ContextStateMachines.States;
+using Sources.InfrastructureInterfaces.Services.InputServices;
+using Sources.PresentationsInterfaces.Views.Character;
+using UnityEngine;
+
+namespace Sources.Controllers.Characters.Movements.States
+{
+    public class CharacterForwardLeftState : ContextStateBase
+    {
+        private readonly CharacterMovement _characterMovement;
+        private readonly ICharacterAnimationView _characterAnimationView;
+        private readonly ICharacterMovementView _characterMovementView;
+        private readonly IInputService _inputService;
+
+        public CharacterForwardLeftState(
+            CharacterMovement characterMovement,
+            ICharacterAnimationView characterAnimationView,
+            ICharacterMovementView characterMovementView,
+            IInputService inputService)
+        {
+            _characterMovement = characterMovement ?? throw new ArgumentNullException(nameof(characterMovement));
+            _characterAnimationView = characterAnimationView ?? throw new ArgumentNullException(nameof(characterAnimationView));
+            _inputService = inputService ?? throw new ArgumentNullException(nameof(inputService));
+            _characterMovementView = characterMovementView ?? throw new ArgumentNullException(nameof(characterMovementView));
+        }
+
+        public override void Enter(object payload = null)
+        {
+            _characterAnimationView.PlayBackwardRight();
+        }
+
+        public override void Exit()
+        {
+        }
+
+        public override void Update(float deltaTime)
+        {
+            float targetSpeed = _inputService.InputData.Speed > 0 
+                ? _inputService.InputData.Speed 
+                : 0.7f;
+
+            _characterMovement.Speed = Mathf.MoveTowards(
+                _characterMovement.Speed, targetSpeed, 0.01f);
+            
+            _characterMovement.Direction = _characterMovement.Speed * 3 *
+                                           deltaTime * _inputService.InputData.MoveDirection.normalized;
+            
+            if(_inputService.InputData.LookPosition == Vector3.zero)
+                return;
+
+            var lookDirection = _inputService.InputData.LookPosition - _characterMovementView.Position;
+            lookDirection.y = _characterMovementView.Position.y;
+            float distance = lookDirection.magnitude;
+            
+            float angle = Vector3.SignedAngle(Vector3.forward, lookDirection, Vector3.up);
+            
+            if(distance < 0.7f)
+                return;
+            
+            _characterMovementView.SetLookRotation(angle);
+        }
+
+    }
+}
--- Assets/Sources/Infrastructure/Factories/Controllers/Characters/CharacterMovementPresenterFactory.cs
+++ Assets/Sources/Infrastructure/Factories/Controllers/Characters/CharacterMovementPresenterFactory.cs
@@ -2,9 +2,13 @@
 using Sources.Controllers.Characters.Movements;
 using Sources.Controllers.Characters.Movements.States;
 using Sources.Domain.Characters;
+using Sources.Domain.Inputs;
+using Sources.Infrastructure.StateMachines.ContextStateMachines.Transitions;
 using Sources.InfrastructureInterfaces.Services.InputServices;
 using Sources.InfrastructureInterfaces.Services.UpdateServices;
 using Sources.PresentationsInterfaces.Views.Character;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Sources.Infrastructure.Factories.Controllers.Characters
 {
@@ -23,14 +27,280 @@ namespace Sources.Infrastructure.Factories.Controllers.Characters
 
         public CharacterMovementPresenter Create(
             CharacterMovement characterMovement,
-            ICharacterMovementView view,
+            ICharacterMovementView characterMovementView,
             ICharacterAnimationView characterAnimationView)
         {
+            if (characterMovement == null) 
+                throw new ArgumentNullException(nameof(characterMovement));
+            
+            if (characterMovementView == null) 
+                throw new ArgumentNullException(nameof(characterMovementView));
+            
+            if (characterAnimationView == null) 
+                throw new ArgumentNullException(nameof(characterAnimationView));
+            
             CharacterIdleState idleState = new CharacterIdleState(
-                characterMovement, view, characterAnimationView, _inputService);
+                characterMovement, characterMovementView, characterAnimationView, _inputService);
+            CharacterForwardState forwardState = new CharacterForwardState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterLeftwardState leftwardState = new CharacterLeftwardState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterRightwardState rightwardState = new CharacterRightwardState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterForwardLeftState forwardLeftState = new CharacterForwardLeftState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterForwardRightState forwardRightState = new CharacterForwardRightState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterBackwardState backwardState = new CharacterBackwardState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterBackwardLeftState backwardLeftState = new CharacterBackwardLeftState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
+            CharacterBackwardRightState backwardRightState = new CharacterBackwardRightState(
+                characterMovement, characterAnimationView, characterMovementView, _inputService);
             
+            FuncContextTransition toForwardTransition = new FuncContextTransition(
+                forwardState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+                    
+                    if (angle > 30 || angle < -30)
+                        return false;
+                    
+                    return true;
+                });
+            idleState.AddTransition(toForwardTransition);
+            forwardRightState.AddTransition(toForwardTransition);
+            forwardLeftState.AddTransition(toForwardTransition);
+            leftwardState.AddTransition(toForwardTransition);
+            rightwardState.AddTransition(toForwardTransition);
+            backwardState.AddTransition(toForwardTransition);
+            backwardLeftState.AddTransition(toForwardTransition);
+            backwardRightState.AddTransition(toForwardTransition);
+
+            FuncContextTransition toForwardRightTransition = new FuncContextTransition(
+                forwardRightState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+                    
+                    if (angle > -30 || angle < -60)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toForwardRightTransition);
+            forwardState.AddTransition(toForwardRightTransition);
+            forwardLeftState.AddTransition(toForwardRightTransition);
+            leftwardState.AddTransition(toForwardRightTransition);
+            rightwardState.AddTransition(toForwardRightTransition);
+            backwardState.AddTransition(toForwardRightTransition);
+            backwardLeftState.AddTransition(toForwardRightTransition);
+            backwardRightState.AddTransition(toForwardRightTransition);
+
+            FuncContextTransition toForwardLeftTransition = new FuncContextTransition(
+                forwardLeftState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    if (angle < 30 || angle > 60)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toForwardLeftTransition);
+            forwardState.AddTransition(toForwardLeftTransition);
+            forwardRightState.AddTransition(toForwardLeftTransition);
+            leftwardState.AddTransition(toForwardLeftTransition);
+            rightwardState.AddTransition(toForwardLeftTransition);
+            backwardState.AddTransition(toForwardLeftTransition);
+            backwardLeftState.AddTransition(toForwardLeftTransition);
+            backwardRightState.AddTransition(toForwardLeftTransition);
+
+            FuncContextTransition toLeftwardTransition = new FuncContextTransition(
+                leftwardState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    if (angle < 60 || angle > 120)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toLeftwardTransition);
+            forwardState.AddTransition(toLeftwardTransition);
+            forwardRightState.AddTransition(toLeftwardTransition);
+            forwardLeftState.AddTransition(toLeftwardTransition);
+            rightwardState.AddTransition(toLeftwardTransition);
+            backwardState.AddTransition(toLeftwardTransition);
+            backwardLeftState.AddTransition(toLeftwardTransition);
+            backwardRightState.AddTransition(toLeftwardTransition);
+
+            FuncContextTransition toRightwardTransition = new FuncContextTransition(
+                rightwardState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    if (angle > -60 || angle < -120)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toRightwardTransition);
+            forwardState.AddTransition(toRightwardTransition);
+            forwardRightState.AddTransition(toRightwardTransition);
+            forwardLeftState.AddTransition(toRightwardTransition);
+            leftwardState.AddTransition(toRightwardTransition);
+            backwardState.AddTransition(toRightwardTransition);
+            backwardLeftState.AddTransition(toRightwardTransition);
+            backwardRightState.AddTransition(toRightwardTransition);
+
+            FuncContextTransition toBackwardLeftTransition = new FuncContextTransition(
+                backwardLeftState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    if (angle < 120 || angle > 150)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toBackwardLeftTransition);
+            forwardState.AddTransition(toBackwardLeftTransition);
+            forwardRightState.AddTransition(toBackwardLeftTransition);
+            forwardLeftState.AddTransition(toBackwardLeftTransition);
+            leftwardState.AddTransition(toBackwardLeftTransition);
+            rightwardState.AddTransition(toBackwardLeftTransition);
+            backwardState.AddTransition(toBackwardLeftTransition);
+            backwardRightState.AddTransition(toBackwardLeftTransition);
+
+            FuncContextTransition toBackwardRightTransition = new FuncContextTransition(
+                backwardRightState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+                    
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    if (angle > -120 || angle < -150)
+                        return false;
+
+                    return true;
+                });
+            idleState.AddTransition(toBackwardRightTransition);
+            forwardState.AddTransition(toBackwardRightTransition);
+            forwardRightState.AddTransition(toBackwardRightTransition);
+            forwardLeftState.AddTransition(toBackwardRightTransition);
+            leftwardState.AddTransition(toBackwardRightTransition);
+            rightwardState.AddTransition(toBackwardRightTransition);
+            backwardState.AddTransition(toBackwardRightTransition);
+            backwardLeftState.AddTransition(toBackwardRightTransition);
+
+            FuncContextTransition toBackwardTransition = new FuncContextTransition(
+                backwardState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+
+                    if (inputData.MoveDirection.magnitude < 0.01f)
+                        return false;
+                    
+                    float angle = Vector3.SignedAngle(
+                        inputData.MoveDirection, characterMovementView.Forward, Vector3.up);
+
+                    //TODO как это работает?
+                    if (angle < 150 && angle > -150)
+                    {
+                        // Debug.Log($"Backward angle: {angle}");
+                        return false;
+                    }
+
+                    return true;
+                });
+            idleState.AddTransition(toBackwardTransition);
+            forwardState.AddTransition(toBackwardTransition);
+            forwardRightState.AddTransition(toBackwardTransition);
+            forwardLeftState.AddTransition(toBackwardTransition);
+            leftwardState.AddTransition(toBackwardTransition);
+            rightwardState.AddTransition(toBackwardTransition);
+            backwardRightState.AddTransition(toBackwardTransition);
+            backwardLeftState.AddTransition(toBackwardTransition);
+
+            FuncContextTransition toIdleTransition = new FuncContextTransition(
+                idleState,
+                context =>
+                {
+                    if (context is not InputData inputData)
+                        return false;
+
+                    if (inputData.MoveDirection.magnitude > 0.01f)
+                        return false;
+
+                    return true;
+                });
+            forwardState.AddTransition(toIdleTransition);
+            forwardRightState.AddTransition(toIdleTransition);
+            forwardLeftState.AddTransition(toIdleTransition);
+            leftwardState.AddTransition(toIdleTransition);
+            rightwardState.AddTransition(toIdleTransition);
+            backwardState.AddTransition(toIdleTransition);
+            backwardRightState.AddTransition(toIdleTransition);
+            backwardLeftState.AddTransition(toIdleTransition);
+
             
-            return new CharacterMovementPresenter(characterMovement, view, _updateRegister, idleState);
+            return new CharacterMovementPresenter(characterMovement, characterMovementView, _updateRegister, idleState);
         }
     }
 }
