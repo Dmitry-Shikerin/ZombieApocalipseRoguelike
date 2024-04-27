424f178454a181108b4b52411f1d3745be8d52b0
alisakorolevamira <105242972+alisakorolevamira@users.noreply.github.com>
2024-04-27 15:37:32 +0300

    change tutorial

--- Assets/Sources/Frameworks/UiFramework/Presentation/Forms/UiCollector.cs
+++ Assets/Sources/Frameworks/UiFramework/Presentation/Forms/UiCollector.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using Sources.Frameworks.UiFramework.Presentation.Buttons;
 using Sources.Presentations.Views;
@@ -10,15 +12,24 @@ namespace Sources.Frameworks.UiFramework.Presentation.Forms
     [DefaultExecutionOrder(-1)]
     public class UiCollector : View
     {
-        [Button(ButtonSizes.Large)] 
-        [FoldoutGroup("UiContainers")] [Required] [SerializeField]
-        private List<UiContainer> _uiContainers;
-        
-        [Button(ButtonSizes.Large)] 
-        [FoldoutGroup("UiButtons")] [Required] [SerializeField]
+        //[Button(ButtonSizes.Large)] 
+        //[FoldoutGroup("UiContainers")] [Required] [SerializeField]
+        //private List<UiContainer> _uiContainers;
+        //
+        //[Button(ButtonSizes.Large)] 
+        //[FoldoutGroup("UiButtons")] [Required] [SerializeField]
+        //private List<UiFormButton> _uiFormButtons;
+
         private List<UiFormButton> _uiFormButtons;
+        private List<UiContainer> _uiContainers;
 
         public IReadOnlyList<UiFormButton> UiFormButtons => _uiFormButtons;
         public IReadOnlyList<IUiContainer> UiContainers => _uiContainers;
+
+        private void Awake()
+        {
+            _uiFormButtons = GetComponentsInChildren<UiFormButton>(true).ToList();
+            _uiContainers = GetComponentsInChildren<UiContainer>(true).ToList();
+        }
     }
 }
