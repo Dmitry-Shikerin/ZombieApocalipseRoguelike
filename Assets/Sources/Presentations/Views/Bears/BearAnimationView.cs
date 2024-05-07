13a2187367d369eff0101c8206f7aab05cf9b162
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-05-07 22:06:27 +0300

    change triggerAnimations

--- Assets/Sources/Presentations/Views/Bears/BearAnimationView.cs
+++ Assets/Sources/Presentations/Views/Bears/BearAnimationView.cs
@@ -13,75 +13,23 @@ namespace Sources.Presentations.Views.Bears
     {
         [Required] [SerializeField] private Animator _animator;
         
-        private static int s_isWalk = Animator.StringToHash("IsWalk");
-        private static int s_isIdle = Animator.StringToHash("IsIdle");
-        private static int s_isAttack = Animator.StringToHash("IsAttack");
-        
-        private readonly List<Action> _stoppingAnimations = new List<Action>();
+        private static int Idle = Animator.StringToHash("Idle");
+        private static int Walk = Animator.StringToHash("Walk");
+        private static int Attack = Animator.StringToHash("Attack");
         
         public event Action Attacking;
 
-        private void Awake()
-        {
-            _stoppingAnimations.Add(StopPlayWalk);
-            _stoppingAnimations.Add(StopPlayIdle);
-            _stoppingAnimations.Add(StopPlayAttack);
-        }
+        public void PlayWalk() =>
+            _animator.SetTrigger(Walk);
 
-        public void PlayWalk()
-        {
-            StopAnimationExcept(StopPlayWalk);
-            
-            _animator.SetBool(s_isWalk, true);
-        }
+        public void PlayIdle() =>
+            _animator.SetTrigger(Idle);
 
-        public void PlayIdle()
-        {
-            StopAnimationExcept(StopPlayIdle);
-            
-            _animator.SetBool(s_isIdle, true);
-        }
+        public void PlayAttack() =>
+            _animator.SetTrigger(Attack);
 
-        public void PlayAttack()
-        {
-            StopAnimationExcept(StopPlayAttack);
-            
-            _animator.SetBool(s_isAttack, true);
-        }
-        
-        private void StopPlayWalk()
-        {
-            if(_animator.GetBool(s_isWalk) == false)
-                return;
-            
-            _animator.SetBool(s_isWalk, false);
-        }
-        
-        private void StopPlayIdle()
-        {
-            if(_animator.GetBool(s_isIdle) == false)
-                return;
-            
-            _animator.SetBool(s_isIdle, false);
-        }
-        
-        private void StopPlayAttack()
-        {
-            if(_animator.GetBool(s_isAttack) == false)
-                return;
-            
-            _animator.SetBool(s_isAttack, false);
-        }
-        
         [UsedImplicitly]
         private void OnAttack() =>
             Attacking?.Invoke();
-
-        private void StopAnimationExcept(Action exceptAction)
-        {
-            _stoppingAnimations
-                .Except(new List<Action>() { exceptAction })
-                .ForEach(action => action.Invoke());
-        }
     }
 }
