e60f039c8426604c27974d1975df110e40c2d67e
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-05-13 12:52:24 +0300

    split UiFrameworkInstaller

--- Assets/Sources/Infrastructure/DIContainers/Gameplay/UiFramework/GameplayUiFrameworkInstaller.cs
+++ Assets/Sources/Infrastructure/DIContainers/Gameplay/UiFramework/GameplayUiFrameworkInstaller.cs
@@ -2,17 +2,7 @@
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Buttons.Handlers;
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Forms;
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Forms.Handlers;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Controllers.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Controllers.Views;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Services.Collectors;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Views.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Views.Forms;
-using Sources.Frameworks.UiFramework.Infrastructure.Services.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Services.Forms;
 using Sources.Frameworks.UiFramework.InfrastructureInterfaces.Commands.Views.Handlers;
-using Sources.Frameworks.UiFramework.Services.Forms;
-using Sources.Frameworks.UiFramework.Services.Localizations;
-using Sources.Frameworks.UiFramework.ServicesInterfaces.Localizations;
 using Zenject;
 
 namespace Sources.Infrastructure.DIContainers.Gameplay.UiFramework
@@ -21,19 +11,19 @@ namespace Sources.Infrastructure.DIContainers.Gameplay.UiFramework
     {
         public override void InstallBindings()
         {
-            Container.Bind<ILocalizationService>().To<LocalizationService>().AsSingle();
-            Container.BindInterfacesAndSelfTo<FormService>().AsSingle();
-            
-            Container.Bind<UiCollectorFactory>().AsSingle();
-
-            Container.Bind<UiButtonFactory>().AsSingle();
-            Container.Bind<UiButtonPresenterFactory>().AsSingle();
-
-            Container.Bind<UiViewFactory>().AsSingle();
-            Container.Bind<UiViewPresenterFactory>().AsSingle();
-
+            // Container.Bind<ILocalizationService>().To<LocalizationService>().AsSingle();
+            // Container.BindInterfacesAndSelfTo<FormService>().AsSingle();
+            //
+            // Container.Bind<UiCollectorFactory>().AsSingle();
+            //
+            // Container.Bind<UiButtonFactory>().AsSingle();
+            // Container.Bind<UiButtonPresenterFactory>().AsSingle();
+            //
+            // Container.Bind<UiViewFactory>().AsSingle();
+            // Container.Bind<UiViewPresenterFactory>().AsSingle();
+            //
             //Buttons
-            Container.Bind<IUiButtonService>().To<UiButtonService>().AsSingle();
+            // Container.Bind<IUiButtonService>().To<UiButtonService>().AsSingle();
             Container.Bind<IButtonCommandHandler>().To<GameplayButtonCommandHandler>().AsSingle();
             
             Container.Bind<ShowFormCommand>().AsSingle();
@@ -51,7 +41,7 @@ namespace Sources.Infrastructure.DIContainers.Gameplay.UiFramework
             Container.Bind<ClearSavesButtonCommand>().AsSingle();
             
             //Views
-            Container.Bind<IUiViewService>().To<UiViewService>().AsSingle();
+            // Container.Bind<IUiViewService>().To<UiViewService>().AsSingle();
             Container.Bind<IUiViewCommandHandler>().To<GameplayUiViewCommandHandler>().AsSingle();
             
             Container.Bind<UnPauseCommand>().AsSingle();
--- Assets/Sources/Infrastructure/DIContainers/MainMenu/UiFramework/MainMenuUiFrameworkInstaller.cs
+++ Assets/Sources/Infrastructure/DIContainers/MainMenu/UiFramework/MainMenuUiFrameworkInstaller.cs
@@ -2,17 +2,7 @@
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Buttons.Handlers;
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Forms;
 using Sources.Frameworks.UiFramework.Infrastructure.Commands.Forms.Handlers;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Controllers.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Controllers.Views;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Services.Collectors;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Views.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Factories.Views.Forms;
-using Sources.Frameworks.UiFramework.Infrastructure.Services.Buttons;
-using Sources.Frameworks.UiFramework.Infrastructure.Services.Forms;
 using Sources.Frameworks.UiFramework.InfrastructureInterfaces.Commands.Views.Handlers;
-using Sources.Frameworks.UiFramework.Services.Forms;
-using Sources.Frameworks.UiFramework.Services.Localizations;
-using Sources.Frameworks.UiFramework.ServicesInterfaces.Localizations;
 using Zenject;
 
 namespace Sources.Infrastructure.DIContainers.MainMenu.UiFramework
@@ -21,19 +11,19 @@ namespace Sources.Infrastructure.DIContainers.MainMenu.UiFramework
     {
         public override void InstallBindings()
         {
-            Container.BindInterfacesAndSelfTo<FormService>().AsSingle();
-            Container.Bind<ILocalizationService>().To<LocalizationService>().AsSingle();
-
-            Container.Bind<UiCollectorFactory>().AsSingle();
-
-            Container.Bind<UiButtonFactory>().AsSingle();
-            Container.Bind<UiButtonPresenterFactory>().AsSingle();
-
-            Container.Bind<UiViewFactory>().AsSingle();
-            Container.Bind<UiViewPresenterFactory>().AsSingle();
+            // Container.BindInterfacesAndSelfTo<FormService>().AsSingle();
+            // Container.Bind<ILocalizationService>().To<LocalizationService>().AsSingle();
+            //
+            // Container.Bind<UiCollectorFactory>().AsSingle();
+            //
+            // Container.Bind<UiButtonFactory>().AsSingle();
+            // Container.Bind<UiButtonPresenterFactory>().AsSingle();
+            //
+            // Container.Bind<UiViewFactory>().AsSingle();
+            // Container.Bind<UiViewPresenterFactory>().AsSingle();
 
             //Buttons
-            Container.Bind<IUiButtonService>().To<UiButtonService>().AsSingle();
+            // Container.Bind<IUiButtonService>().To<UiButtonService>().AsSingle();
             Container.Bind<IButtonCommandHandler>().To<MainMenuButtonCommandHandler>().AsSingle();
 
             Container.Bind<ShowFormCommand>().AsSingle();
@@ -46,7 +36,7 @@ namespace Sources.Infrastructure.DIContainers.MainMenu.UiFramework
             Container.Bind<ClearSavesButtonCommand>().AsSingle();
 
             //Views
-            Container.Bind<IUiViewService>().To<UiViewService>().AsSingle();
+            // Container.Bind<IUiViewService>().To<UiViewService>().AsSingle();
             Container.Bind<IUiViewCommandHandler>().To<MainMenuUiViewCommandHandler>().AsSingle();
 
             Container.Bind<UnPauseCommand>().AsSingle();
