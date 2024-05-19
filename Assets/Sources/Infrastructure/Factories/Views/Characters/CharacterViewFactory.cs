949de4c3006f661599a564aba79c5ff90765d295
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2024-05-19 16:01:17 +0300

    add instantiale for SawLauncherAbility

--- Assets/Sources/Infrastructure/Factories/Views/Characters/CharacterViewFactory.cs
+++ Assets/Sources/Infrastructure/Factories/Views/Characters/CharacterViewFactory.cs
@@ -83,7 +83,11 @@ namespace Sources.Infrastructure.Factories.Views.Characters
             _characterHealthViewFactory.Create(character.CharacterHealth, characterView.CharacterHealthView);
             _healthUiFactory.Create(character.CharacterHealth, _gameplayHud.CharacterHealthUi);
 
-            SawLauncherAbilityView sawLauncherAbilityView = Object.FindObjectOfType<SawLauncherAbilityView>();
+            // SawLauncherAbilityView sawLauncherAbilityView = Object.FindObjectOfType<SawLauncherAbilityView>();
+            SawLauncherAbilityView sawLauncherAbilityView = Object.Instantiate(
+                Resources.Load<SawLauncherAbilityView>("Views/SawLauncherAbilityView"),
+                _rootGameObject.CharacterSpawnPoint.Position,
+                Quaternion.identity);
             sawLauncherAbilityView.SetTargetFollow(characterView.transform);
 
             for (int i = 0; i < sawLauncherAbilityView.SawLauncherViews.Count; i++)
